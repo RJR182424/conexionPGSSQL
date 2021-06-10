8bd4543a7e5ce6e994ffa3242c49d5af3cf52228
RJR182424 <robertjes.389@gmail.com>
2021-06-10 13:58:15 -0500

    Agregar archivos de proyecto.

---
+++ conexionPGSSQL/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace conexionPGSSQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Conectar = new System.Windows.Forms.Button();
+            this.Desconectar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Conectar
+            // 
+            this.Conectar.Location = new System.Drawing.Point(12, 184);
+            this.Conectar.Name = "Conectar";
+            this.Conectar.Size = new System.Drawing.Size(188, 50);
+            this.Conectar.TabIndex = 0;
+            this.Conectar.Text = "Conectar";
+            this.Conectar.UseVisualStyleBackColor = true;
+            this.Conectar.Click += new System.EventHandler(this.Conectar_Click);
+            // 
+            // Desconectar
+            // 
+            this.Desconectar.Location = new System.Drawing.Point(12, 240);
+            this.Desconectar.Name = "Desconectar";
+            this.Desconectar.Size = new System.Drawing.Size(188, 50);
+            this.Desconectar.TabIndex = 1;
+            this.Desconectar.Text = "Desconectar";
+            this.Desconectar.UseVisualStyleBackColor = true;
+            this.Desconectar.Click += new System.EventHandler(this.Desconectar_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::conexionPGSSQL.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(188, 166);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(207, 295);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Desconectar);
+            this.Controls.Add(this.Conectar);
+            this.Name = "Form1";
+            this.Text = "C.P.SQL";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Conectar;
+        private System.Windows.Forms.Button Desconectar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ conexionPGSSQL/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace conexionPGSSQL
+{
+    public partial class Form1 : Form
+    {
+        public NpgsqlConnection conexion = new NpgsqlConnection() {};
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Conectar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexion.ConnectionString = "Username = postgres; Password = admin; Host = localhost; Port = 5432; Database = Turnos";
+                conexion.Open();
+                MessageBox.Show("Ya se conecto");
+            }
+            catch (Exception x) {
+                MessageBox.Show(x.Message);
+            }
+        }
+
+        private void Desconectar_Click(object sender, EventArgs e)
+        {
+            try {
+                if (conexion.State == ConnectionState.Open)
+                {
+                    conexion.Close();
+                    MessageBox.Show("Ya se desconecto");
+                }
+                else {
+                    MessageBox.Show("no esta conectada wey! xd");
+                }
+            } catch (Exception x) {
+                MessageBox.Show(x.Message);
+            }
+        }
+    }
+}
